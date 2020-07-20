2811556014a3dda78cc8950e06bf50635440919f
yashogoz <yashogoz@yahoo.co.jp>
2020-07-20 22:53:18 +0900

    Initial Commit

---
+++ TechAcademy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechAcademy
+//
+//  Created by 山田祥吾 on 2020/07/20.
+//  Copyright © 2020 yashogoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
